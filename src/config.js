360198195d17811e4bc52488dd12137c4248c1a5
simon <simon@uwotech.com>
2020-05-24 18:50:16 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export const host = '' // 线上
 // export const host = 'http://192.168.0.119:28080' // 局域网
 
 export const dhost = 'http://47.97.110.251:18080' // 线上
-// export const dhost = 'http://192.168.0.119:18080' // 本地
+// export const dhost = 'http://192.168.0.102:18080' // 本地
 // export const dhost = 'http://47.98.101.206:18080' // 测试
 
